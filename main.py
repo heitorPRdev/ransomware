afddff5f80447e9a808f3147a1426f27f02c92c5
heitorPrDev <herohecttorplay@gmail.com>
2024-06-27 21:15:54 -0300

    0.0.2-Versão Base-

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from cryptography.fernet import Fernet
-
+import flet as ft
 files = []
 for file in os.listdir():
     if file == 'main.py' or file == 'chave.key':
@@ -10,34 +10,47 @@ for file in os.listdir():
 
 
 
-while True:
-    try:
-        escolha = int(input('Escolha uma opção\n[1] Cryptografar\n[2] Descryptografar\n[3] Exit\n>> '))
-        if escolha == 1:
-            key = Fernet.generate_key()
-            with open('chave.key', 'wb') as chave:            
-                chave.write(key)
+            
+def main(page: ft.Page):
+    def cryptografando(e):
+        key = Fernet.generate_key()
+        with open('chave.key', 'wb') as chave:            
+            chave.write(key)
             for file in files:
-                
+                    
                 with open(file,'rb') as arquivo:
                     contents = arquivo.read()
-                    
+                            
                 contents_encryp = Fernet(key).encrypt(contents)
-                
+                        
                 with open(file, 'wb') as arquivo:
                     arquivo.write(contents_encryp)
-                    print(f'\nArquivo {file} criptografado com sucesso\n')
-        if escolha == 2:
-            with open('chave.key','rb') as key:
-                secretkey = key.read()
-            for file in files:
-                with open(file, 'rb') as thefile:
-                    content = thefile.read()
-                content_decrypt = Fernet(secretkey).decrypt(content)
-                with open(file,'wb') as thefile:
-                    thefile.write(content_decrypt)
-                    print(f'\nArquivo {file} descriptografando com sucesso\n')
-        if escolha == 3:
-            break
-    except ValueError:
-        print('\ncoloque uma opção valida\n')
+    def descryptografando(e):
+        with open('chave.key','rb') as key:
+            secretkey = key.read()
+        for file in files:
+            with open(file, 'rb') as thefile:
+                content = thefile.read()
+            content_decrypt = Fernet(secretkey).decrypt(content)
+            with open(file,'wb') as thefile:
+                thefile.write(content_decrypt)
+
+    text = ft.Text('Escolha uma opção')
+    btnCryp = ft.FilledButton('Cryptografar',on_click=cryptografando)
+    btnDescrypt = ft.FilledButton('Descryptografar', on_click=descryptografando)
+    
+    page.add(
+        text,
+        ft.Row([
+            ft.Container(
+                content=btnCryp,
+            ),
+            ft.Container(
+                content=btnDescrypt,
+            )
+        ]),
+    )
+
+
+
+ft.app(target=main)
